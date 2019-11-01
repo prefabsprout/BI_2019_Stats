45019953385e9fafcacd8a84c1794d37c542f410
Stepan Petrosian <stephenpetrosian@gmail.com>
2019-11-02 00:12:50 +0300

    Update correlation.R

--- correlation.R
+++ correlation.R
@@ -1,19 +1,21 @@
-correl <- function(x,y, method = 'pearson'){
+correl <- function(df, method = 'pearson'){
   if (method == 'pearson'){
-    answer <- cov(x, y) / (sd(x) * sd(y))
+    answer <- cov(df[,1], df[,2]) / (sd(df[,1]) * sd(df[,2]))
     return(answer)
   }
   else if (method == 'spearman'){
-    answer <- cov(rank(x), rank(y)) / (sd(rank(x)) * sd(rank(y)))
+    answer <- cov(rank(df[,1]), rank(df[,2])) / (sd(rank(df[,1])) * sd(rank(df[,2])))
     return(answer)
   }
 }
 
 
-## Comparing results of my function to basic package's results
+## Comparing results of mdf[,2] function to basic package's results
 
-correl(mtcars$mpg, mtcars$hp)
-correl(mtcars$mpg, mtcars$hp, method = 'spearman')
+test_data <- data.frame(mtcars$mpg, mtcars$hp)
 
-cor(mtcars$mpg, mtcars$hp)
-cor(mtcars$mpg, mtcars$hp, method = 'spearman')
+correl(test_data)
+correl(test_data, method = 'spearman')
+
+cor(test_data[,1], test_data[,2])
+cor(test_data[,1], test_data[,2], method = 'spearman')
